2101b0d7f5e1c04a9f35550ed62c5ec8b5453592
Felipe Armando Sotelo Sotelo <felipe.sotelo@live.com>
2023-11-29 11:30:47 -0500

    upload app

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,13 +4,15 @@ import TodoList from './components/TodoListProject/TodoList';
 import Weather from "./components/WeatherProject/Weather"
 import DisplayClocks from './components/ClockWorldProject/DisplayClocks';
 import PasswordGenerator from './components/PasswordGeneratorProject/PasswordGenerator';
+import Gallery from './components/ImageGaleryProject/Gallery';
 function App() {
   return (
     <div className="App">
       {/* <TodoList/>     */}
       {/* <Weather/> */}
       {/* <DisplayClocks/> */}
-      <PasswordGenerator/>
+      {/* <PasswordGenerator/> */}
+      <Gallery/>
     </div>
   );
 }
