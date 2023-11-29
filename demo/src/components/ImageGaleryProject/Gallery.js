7bea07eb3ab9100517861c15d41f7be983f9d785
Felipe Armando Sotelo Sotelo <felipe.sotelo@live.com>
2023-11-29 11:49:27 -0500

    correction app

--- demo/src/components/ImageGaleryProject/Gallery.js
+++ demo/src/components/ImageGaleryProject/Gallery.js
@@ -1,17 +1,25 @@
 // src/Gallery.js
 import React, { useState } from 'react';
 import './styles/Gallery.css'; // Asegúrate de tener un archivo CSS para los estilos
-import image1 from './images/image1.jpeg'
-import image2 from './images/image1.jpeg'
-import image3 from './images/image1.jpeg'
-import image4 from './images/image1.jpeg'
-import image5 from './images/image1.jpeg'
+import image1 from './images/image1.png'
+import image2 from './images/image2.png'
+import image3 from './images/image3.png'
+import image4 from './images/image4.png'
+import image5 from './images/image5.png'
+import image6 from './images/image6.png'
+import image7 from './images/image7.png'
+import image8 from './images/image8.png'
+import image9 from './images/image9.png'
 const images = [
   image1,
   image2,
   image3,
   image4,
   image5,
+  image6,
+  image7,
+  image8,
+  image9
   // Agrega más imágenes según sea necesario
 ];
 
